6ba817435315d26fdfbf47606013f4e2c5f631fe
ezeifunanyaeze-jpg <ezeifunanyaeze@gmail.com>
2026-05-22 14:17:28 +0100

    use render port environment variable

--- backend/server.js
+++ backend/server.js
@@ -257,7 +257,7 @@ app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log("Server running on port 5000");
